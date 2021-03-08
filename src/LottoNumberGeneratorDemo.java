119f3c625d3bb5d7fa54e99452f7661326b49e86
MirjamWeirer <mirjam.weirer@gmx.at>
2021-03-08 12:05:53 +0100

    Lotto / Zufallszahl / verschachtelte Schleifen

--- src/LottoNumberGeneratorDemo.java
+++ src/LottoNumberGeneratorDemo.java
@@ -6,23 +6,27 @@ public class LottoNumberGeneratorDemo {
 
         System.out.println("Unser Quicktipps: ");
 
-        System.out.println(generateOneNumber(45));
-        System.out.println(generateOneNumber(45));
-        System.out.println(generateOneNumber(45));
-        System.out.println(generateOneNumber(45));
-        System.out.println(generateOneNumber(45));
-        System.out.println(generateOneNumber(45));
-
-
+        //Verschachtelung von Schleifen
+        for (int tipps = 0; tipps < 5; tipps ++){
+            gerateOneTipp();
+        }
+    }
+    public static void gerateOneTipp(){
+        for (int i = 0; i < 6; i++) {
+            System.out.print(generateOneNumber(45)+ ", ");
+        }
+        System.out.println();
     }
 
     public static int generateOneNumber(int range){
         Random random = new Random(); //Random = Klasse; zufall
-        int value = (random.nextInt() % range) + 1;
-        if (value < 0){
-            value = value * -1;
+        int randValue = random.nextInt();
+
+        if (randValue < 0){
+            randValue = randValue * -1;
         }
 
+        int value = (randValue % range) + 1;
         return value;
     }
 }
