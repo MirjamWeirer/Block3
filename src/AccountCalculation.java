33a60f00fc63786444122f5e8a070536481d8b2b
MirjamWeirer <mirjam.weirer@gmx.at>
2021-03-08 11:27:14 +0100

    Zinsesberechnung

--- src/AccountCalculation.java
+++ src/AccountCalculation.java
@@ -3,17 +3,16 @@ public class AccountCalculation {
         double startCapital = 1000.0;
         double result;
         for (int i = 1; i <= 10 ; i++) {
+
             result = startCapital * 1.015;
-            if (i <= 9){
+            if (i <= 9) {
                 System.out.println("End of " + i + " year " + startCapital + " -> " + result);
-            }
-            else {
+            } else {
                 System.out.println("End of " + i + " year " + startCapital + " -> " + result);
                 System.out.println("Total: " + result);
             }
             startCapital = result;
         }
     }
-    //todo in methode schreiben
-    //das startkapital, jahre, zinsen verändert werden können
+
 }
